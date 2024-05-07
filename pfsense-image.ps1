d011bafffc0ffff60ff47592aba4d982b55e8a1e
mavelot <127770209+mavelot@users.noreply.github.com>
2024-05-07 13:16:02 +0200

    Add files via upload

---
+++ pfsense-image.ps1
@@ -0,0 +1,147 @@
+# pfSense to AWS Ec2 image import
+# Author: Marco Velotto
+# Last update: 2024-05-03
+#
+# Put the image file in the image-input folder and follow istructions on screen
+
+$Timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$RandStr = -join ((48..57) + (97..122) | Get-Random -Count 8 | ForEach-Object {[char]$_})
+
+Clear-Host
+
+Write-Host "###########################################################" -ForegroundColor green
+Write-Host "#               pfSense to AWS Ec2 Image import           #" -ForegroundColor green
+Write-Host "###########################################################" -ForegroundColor green
+Write-Host "`n"
+
+
+$Profile = "default"
+$Profile = Read-Host -Prompt "Please enter your AWS profile (enter for default value) [$($Profile)]"
+if ([string]::IsNullOrEmpty($Profile)) { $Profile = "default" }
+
+$Bucket = "pfsense-memstick"
+$Bucket = Read-Host -Prompt "Please enter your S3 Bucket Name (enter for default value) [$($Bucket)]"
+if ([string]::IsNullOrEmpty($Bucket)) { $Bucket = "pfsense-memstick" }
+Write-Host ""
+
+# Print command for configuring the aws profile
+Write-Host "aws configure --profile $Profile"
+aws configure --profile $Profile
+Write-Host ""
+
+# create own specific json for roles
+$RolePolicy = "role-policy_$Timestamp.json"
+
+Copy-Item -Path "./aws/json/role-policy.json" -Destination $RolePolicy
+(Get-Content -Path $RolePolicy) -replace "BUCKET_PLACEHODLER", $Bucket | Set-Content -Path $RolePolicy
+
+Write-Host "Delete vmimport role and policy if exist. Errors is normal if they not exist..."
+aws iam delete-role-policy --profile $Profile --role-name vmimport --policy-name vmimport
+aws iam delete-role --profile $Profile --role-name vmimport
+
+Write-Host "Create role and policy vmimport for your S3 Bucket..."
+$RoleId = (aws iam create-role --profile $Profile --role-name vmimport --assume-role-policy-document file://aws/json/trust-policy.json | ConvertFrom-Json).Role.RoleId
+
+if ([string]::IsNullOrEmpty($RoleId)) {
+    Write-Host "`nError in role creation. Please verify IAM panel on your AWS console."
+    Remove-Item -Path $RolePolicy
+    exit
+}
+else {
+    Write-Host "Role created, RoleId: $RoleId"
+}
+
+aws iam put-role-policy --profile $Profile --role-name vmimport --policy-name vmimport --policy-document file://$RolePolicy
+Remove-Item -Path $RolePolicy
+
+Write-Host "Wait 10 seconds to ensure role and policy propagation"
+
+for ($i = 1; $i -le 10; $i++) {
+    Write-Host -NoNewline "$i." -ForegroundColor red
+    Start-Sleep -Seconds 1
+}
+
+$Image = Get-ChildItem -Path "./input-image/" | Sort-Object -Property LastWriteTime | Select-Object -Last 1 | Select-Object -ExpandProperty Name
+
+Write-Host "`nCopy USB Installer image to S3 Bucket..." -ForegroundColor green
+#aws s3 cp --profile $Profile "./input-image/$Image" "s3://$Bucket/$Image"
+
+# Create own specific json for import
+$ImportSnapshot = "import-snapshot_$Timestamp.json"
+
+Copy-Item -Path "./aws/json/import-snapshot.json" -Destination $ImportSnapshot
+(Get-Content -Path $ImportSnapshot) -replace "FORMAT_PLACEHODLER", "RAW" | Set-Content -Path $ImportSnapshot
+(Get-Content -Path $ImportSnapshot) -replace "BUCKET_PLACEHODLER", $Bucket | Set-Content -Path $ImportSnapshot
+(Get-Content -Path $ImportSnapshot) -replace "IMAGE_PLACEHODLER", $Image | Set-Content -Path $ImportSnapshot
+
+Write-Host "`nImport installer image as EC2 snapshot..."
+
+# Capture ImporTaskId
+$ImportTaskId = (aws ec2 --profile $Profile import-snapshot --disk-container file://$ImportSnapshot | ConvertFrom-Json).ImportTaskId
+
+if ([string]::IsNullOrEmpty($ImportTaskId)) {
+    Write-Host "`nError in import task creation. Please verify permissions on your AWS console" -ForegroundColor red
+    Remove-Item -Path $ImportSnapshot
+    exit
+}
+else {
+    Write-Host "Import task correctly created, ImportTaskId: $ImportTaskId"
+    Remove-Item -Path $ImportSnapshot
+}
+
+# Follow Progress
+
+$ImportProgress = 0
+Write-Host "Import progress:"
+
+while ($ImportProgress -lt 99) {
+    $ImportProgress = (aws ec2 --profile $Profile describe-import-snapshot-tasks --import-task-id $ImportTaskId | ConvertFrom-Json).ImportSnapshotTasks[0].SnapshotTaskDetail.Progress
+    if ([string]::IsNullOrEmpty($ImportProgress)) {
+        $ImportProgress = 100
+    }
+    else {
+        Write-Host -NoNewline "`r$ImportProgress% " -ForegroundColor Red
+        for ($i = 1; $i -le 10; $i++) {
+            Write-Host -NoNewline ">"  -ForegroundColor Red
+            Start-Sleep -m 500
+        }
+        Write-Host -NoNewline "`r$ImportProgress%            " -ForegroundColor Red
+    }
+}
+
+# Obtain final Status of Import Task
+
+$Status = (aws ec2 --profile $Profile describe-import-snapshot-tasks --import-task-id $ImportTaskId | ConvertFrom-Json).ImportSnapshotTasks[0].SnapshotTaskDetail.Status
+
+if ($Status -eq "completed") {
+    Write-Host -NoNewline "`r$ImportProgress%" -ForegroundColor Green
+    Write-Host "`nSnapshot import completed successfully !" -ForegroundColor Green
+    $SnapshotId = (aws ec2 --profile $Profile describe-import-snapshot-tasks --import-task-id $ImportTaskId | ConvertFrom-Json).ImportSnapshotTasks[0].SnapshotTaskDetail.SnapshotId
+}
+Write-Host "SnapshotID: $SnapshotId"
+
+$AmiName = "ami-$RandStr- pfSense 2.7.2 CE"
+$AmiName = Read-Host -Prompt "Please enter desidered name for AMI (enter for default value) [$($AmiName)]"
+if ([string]::IsNullOrEmpty($AmiName)) { $AmiName = "ami-$RandStr- pfSense 2.7.2 CE" }
+
+$AmiDesc = "pfSense AMI from snapshot $SnapshotId"
+$AmiDesc = Read-Host -Prompt "Please enter a description for AMI (enter for default value) [$($AmiDesc)]"
+if ([string]::IsNullOrEmpty($AmiDesc)) { $AmiDesc = "AMI from snapshot $SnapshotId" }
+Write-Host ""
+
+# Create own specific json for AMI register
+$DeviceMapping = "device-mapping_$Timestamp.json"
+
+Copy-Item -Path "./aws/json/device-mapping.json" -Destination $DeviceMapping
+(Get-Content -Path $DeviceMapping) -replace "SNAPSHOT_PLACEHOLDER", $SnapshotId | Set-Content -Path $DeviceMapping
+
+$ImageId = (aws ec2 register-image --profile $Profile --name "$AmiName" --description "$AmiDesc" --architecture x86_64 --ena-support --boot-mode uefi --root-device-name /dev/xvda --virtualization-type hvm --block-device-mappings file://$DeviceMapping | ConvertFrom-Json).ImageId
+
+if ([string]::IsNullOrEmpty($ImageId)) {
+    Write-Host "`nError in image registration. Please verify on your AWS console." -ForegroundColor Red
+}
+else {
+    Write-Host "`nSuccessfully created your pfSense AMI with Id: $ImageId" -ForegroundColor Green
+    Write-Host "Now you can configure and launch your pfSense instance !" -ForegroundColor Green
+}
+Remove-Item -Path $DeviceMapping
---
+++ pfsense-image.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# pfSense to AWS Ec2 image import
+# Author: Marco Velotto
+# Last update: 2024-05-03
+#
+# Put the image file in the image-input folder and follow istructions on screen
+
+
+TIMESTAMP="$(date +%Y%M%d_%H%M%S)"
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+RANDSTR=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
+
+clear
+echo -e "${GREEN}###########################################################"
+echo -e "#               pfSense to AWS Ec2 Image import           #"
+echo -e "###########################################################${NC}"
+echo -e "\n"
+
+
+PROFILE="default"
+read -e -i "$PROFILE" -p "Please enter your AWS profile (enter for default): " input
+PROFILE="${input:=default}"
+
+BUCKET="pfsense-memstick"
+read -e -i "$BUCKET" -p "Please enter your S3 Bucket Name (modify and press enter): " input
+BUCKET="${input:=pfsense-memstick}"
+echo -e "\n"
+
+# print command for configuring the aws profile
+echo "aws configure --profile ${PROFILE}"
+aws configure --profile ${PROFILE}
+echo -e "\n"
+
+
+# create own specific json for roles
+
+ROLEPOLICY="role-policy_${TIMESTAMP}.json"
+
+cp ./aws/json/role-policy.json ${ROLEPOLICY}
+sed -i s/BUCKET_PLACEHODLER/${BUCKET}/g ${ROLEPOLICY}
+
+echo "Delete vmimport role and policy if exist. Errors is normal if they not exist..."
+aws iam delete-role-policy --profile ${PROFILE} --role-name vmimport --policy-name vmimport
+aws iam delete-role --profile ${PROFILE} --role-name vmimport
+
+echo -e "\nCreate role and policy vmimport for your S3 Bucket..."
+ROLEID=$(aws iam create-role --profile ${PROFILE} --role-name vmimport --assume-role-policy-document file://aws/json/trust-policy.json | tee /dev/null | grep RoleId | cut -d \" -f 4)
+
+if [ -z "$ROLEID" ]
+then
+      echo -e "\n${RED}Error in role creation. Please verify IAM panel on your AWS console.${NC}"
+      rm ${ROLEPOLICY}
+      exit
+else
+      echo -e "Role created, RoleId: ${ROLEID}"
+fi
+
+aws iam put-role-policy --profile ${PROFILE} --role-name vmimport --policy-name vmimport --policy-document file://${ROLEPOLICY}
+rm ${ROLEPOLICY}
+
+echo "Wait 10 seconds to ensure role and policy propagation"
+
+for i in {1..10..1}
+do
+   echo -ne "${RED}$i.${NC}"
+   sleep 1
+done
+
+IMAGE="$(ls -1tr ./input-image/ | tail -n1)"
+
+echo -e "\n${GREEN}Copy USB Installer image to S3 Bucket...${NC}"
+#aws s3 cp --profile ${PROFILE} ./input-image/${IMAGE} s3://${BUCKET}/${IMAGE}
+
+# Create own specific json for import
+IMPORTSNAPSHOT="import-snapshot_${TIMESTAMP}.json"
+
+cp ./aws/json/import-snapshot.json ${IMPORTSNAPSHOT}
+sed -i s/FORMAT_PLACEHODLER/RAW/g ${IMPORTSNAPSHOT}
+sed -i s/BUCKET_PLACEHODLER/${BUCKET}/g ${IMPORTSNAPSHOT}
+sed -i s/IMAGE_PLACEHODLER/${IMAGE}/g ${IMPORTSNAPSHOT}
+
+
+echo -e "\nImport installer image as EC2 snapshot..."
+
+# print output to stdout, capture ImporTaskId
+IMPORTTASKID=$(aws ec2 --profile ${PROFILE} import-snapshot --disk-container file://${IMPORTSNAPSHOT} | tee /dev/null | grep ImportTaskId | cut -d \" -f 4)
+
+if [ -z "$IMPORTTASKID" ]
+then
+      echo -e "\n${RED}Error in import task creation. Please verify permissions on your AWS console.${NC}"
+	#rm ${IMPORTSNAPSHOT}
+  	exit
+else
+      echo -e "Import task correctly created, ImportTaskId: ${IMPORTTASKID}"
+      rm ${IMPORTSNAPSHOT}
+fi
+
+PROGRESS=0
+echo -ne "Import progress:\n"
+
+while [  $PROGRESS -lt 99 ]; do
+    PROGRESS=$(aws ec2 --profile ${PROFILE} describe-import-snapshot-tasks --import-task-id ${IMPORTTASKID}| tee /dev/null | grep Progress | cut -d \" -f 4)
+    if [ -z "$PROGRESS" ]
+     then
+       PROGRESS=100
+     else
+       printf "${RED}\r$PROGRESS%% "
+       for i in {1..10..1}
+        do
+         echo -ne ">"
+         sleep 0.5
+        done
+       printf "\r$PROGRESS%%            ${NC}"
+     fi
+done
+
+STATUS=$(aws ec2 --profile ${PROFILE} describe-import-snapshot-tasks --import-task-id ${IMPORTTASKID}| tee /dev/null | grep Status | cut -d \" -f 4)
+
+if [ ${STATUS} == "completed" ]
+ then
+   printf "\r${GREEN}$PROGRESS%%           "
+   echo -e "\nSnapshot import completed successfully !${NC}"
+   SNAPSHOTID=$(aws ec2 --profile ${PROFILE} describe-import-snapshot-tasks --import-task-id ${IMPORTTASKID}| tee /dev/null | grep SnapshotId | cut -d \" -f 4)
+fi
+echo -e "\nSnapshotID: ${SNAPSHOTID}"
+
+AMINAME="ami-${RANDSTR}- pfSense 2.7.2 CE"
+read -e -i "$AMINAME" -p "Please enter desidered name for AMI (modify and press enter): " input
+AMINAME="${input:=ami-${RANDSTR}- pfSense 2.7.2 CE}"
+
+AMIDESC="pfSense AMI from snapshot ${SNAPSHOTID}"
+read -e -i "$AMIDESC" -p "Please enter a description for AMI (modify and press enter): " input
+AMIDESC="${input:=AMI from snapshot ${SNAPSHOTID}}"
+echo -e "\n"
+
+# Create own specific json for AMI register
+DEVICEMAPPING="device-mapping_${TIMESTAMP}.json"
+
+cp ./aws/json/device-mapping.json ${DEVICEMAPPING}
+sed -i s/SNAPSHOT_PLACEHOLDER/${SNAPSHOTID}/g ${DEVICEMAPPING}
+
+IMAGEID=$(aws ec2 register-image --profile ${PROFILE} --name "${AMINAME}" --description "${AMIDESC}" \
+ --architecture x86_64  --ena-support  --boot-mode uefi --root-device-name /dev/xvda \
+ --virtualization-type hvm --block-device-mappings file://${DEVICEMAPPING} | tee /dev/null | grep ImageId | cut -d \" -f 4)
+
+if [ -z "$IMAGEID" ]
+then
+      echo -e "\n${RED}Error in image registration. Please verify on your AWS console.${NC}"
+else
+      echo -e "\nSuccessfully created your pfSense AMI with Id: ${IMAGEID}"
+      echo -e "${GREEN}Now you can configure and launch your pfSense instance !${NC}"
+fi
+rm ${DEVICEMAPPING}
+
+exit
